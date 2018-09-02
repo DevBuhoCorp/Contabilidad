508cd909f11bd5e0a7280bdb164b406e400e3ac5
kbsg <kebryansg@gmail.com>
2018-09-02 00:05:09 -0500

    Agregar Compoenente View

---
+++ src/app/modulos/configurar/modelospc/viewmodelopc/viewmodelopc.component.ts
@@ -0,0 +1,16 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-viewmodelopc',
+  templateUrl: './viewmodelopc.component.html',
+  styles: []
+})
+export class ViewmodelopcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
