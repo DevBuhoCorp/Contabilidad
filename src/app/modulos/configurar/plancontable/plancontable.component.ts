0c8f247511806a51992bd11d6aea4799623e4874
kbsg <kebryansg@gmail.com>
2018-09-05 23:04:26 -0500

    popup

--- src/app/modulos/configurar/plancontable/plancontable.component.ts
+++ src/app/modulos/configurar/plancontable/plancontable.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
-import { MatDialogRef, MatDialog, MatSnackBar } from '@angular/material';
-import { Subscription } from 'rxjs/Subscription';
-import { TreeNode } from 'primeng/components/common/api';
-import { PopupComponentPC } from './popup/popup.component';
-import { AppLoaderService } from '../../../shared/servicios/app-loader/app-loader.service';
-import { AppConfirmService } from '../../../shared/servicios/app-confirm/app-confirm.service';
-import { CrudService } from '../../../shared/servicios/crud.service';
-import { PlanContable } from './plancontable.model';
-import { ModeloPlanContable } from '../modelospc/modelopc.model';
+import {Component, OnInit, OnDestroy, ViewEncapsulation} from '@angular/core';
+import {MatDialogRef, MatDialog, MatSnackBar} from '@angular/material';
+import {Subscription} from 'rxjs/Subscription';
+import {TreeNode} from 'primeng/components/common/api';
+import {PopupComponentPC} from './popup/popup.component';
+import {AppLoaderService} from '../../../shared/servicios/app-loader/app-loader.service';
+import {AppConfirmService} from '../../../shared/servicios/app-confirm/app-confirm.service';
+import {CrudService} from '../../../shared/servicios/crud.service';
+import {PlanContable} from './plancontable.model';
+import {ModeloPlanContable} from '../modelospc/modelopc.model';
+import {CuentaContable} from './cuentacontable.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-plancontable',
@@ -26,23 +28,26 @@ export class PlancontableComponent implements OnInit, OnDestroy {
   Cuentas: any[];
   public items: any[];
   public getItemSub: Subscription;
+
   constructor(
     private dialog: MatDialog,
+    private httpClient: HttpClient,
     private snack: MatSnackBar,
     private crudService: CrudService,
     private loader: AppLoaderService,
     private confirmService: AppConfirmService,
   ) {
-    this.crudService.ListarDatos("modeloplancontable", "All", 0).map((response) => {
+    this.crudService.ListarDatos('modeloplancontable', 'All', 0).map((response) => {
       return response.json() as ModeloPlanContable[];
     }).toPromise().then(x => {
       this.Modelos = x;
-    })
+    });
   }
 
   ngOnInit() {
 
   }
+
   CargarPlan() {
     this.crudService.ListarDatos('plancontable', 'ALL', this.selectedValue).map((response) => {
       return response.json();
@@ -50,74 +55,108 @@ export class PlancontableComponent implements OnInit, OnDestroy {
       this.filesTree0 = JSON.parse(x[0].data) as TreeNode[];
     });
   }
-  CargarCuenta(id){
-    this.crudService.ListarDatos("cuentacontable", "ID", id).map((response) => {
-      return response.json();
-    }).toPromise().then(x => {
-      this.Cuentas = x;
-      console.log(this.Cuentas);
-    })
 
-  }
   ngOnDestroy() {
     if (this.getItemSub) {
       this.getItemSub.unsubscribe();
     }
   }
 
-  openPopUp(data: any = {}, isNew?) {
+  async openPopUp(data: any = {}, isNew?) {
+    // data.promise = await this.httpClient.get(`http://localhost:8000/api/plancontable/${ this.selectedValue }/cuentacontable/${ data.data }`).toPromise();
     let title = isNew ? 'Agregar' : 'Actualizar';
-    /*this.CargarCuenta(data.data);
-    console.log(this.Cuentas);*/
+    data.promise = undefined;
+    if (!isNew)
+      data.promise = await this.httpClient.get(`http://localhost:8000/api/cuentacontable/${ data.data }`).toPromise();
+    else
+      data.promise = await this.httpClient.get(`http://localhost:8000/api/plancontable/${ this.selectedValue }/cuentacontable/${ data.data }`).toPromise();
+
     let dialogRef: MatDialogRef<any> = this.dialog.open(PopupComponentPC, {
       width: '720px',
       disableClose: true,
-      data: { title: title, payload: data }
-    })
-    dialogRef.afterClosed()
-      .subscribe(res => {
-        if (!res) {
-          // If user press cancel
-          return;
-        }
-        this.loader.open();
-        if (isNew) {
-          this.crudService.Insertar(res, "modeloplancontable/").subscribe(data => {
-            // this.getItems("All", 0);
-            this.loader.close();
-            this.snack.open('Agregado!', 'OK', { duration: 4000 })
-          })
-        } else {
-          this.crudService.Actualizar(data.ID, res, "modeloplancontable/").subscribe(data => {
-            //  this.getItems("All", 0);
-            this.loader.close();
-            this.snack.open('Actualizado!', 'OK', { duration: 4000 })
-          })
-        }
-      })
+      data: {title: title, payload: data}
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      // res => Etiqueta , Estado
+      if (!res) return;
+
+      if (isNew) {
+        // let ccontable: CuentaContable = {
+        //   ID: 0,
+        //   NumeroCuenta: 0,
+        //   Etiqueta: '',
+        //   IDGrupo: 0,
+        //   Grupo: 0,
+        //   IDPadre: 0,
+        //   Saldo: 0,
+        //   Estado: 'ACT'
+        //
+        // };
+        console.log(Object.assign(res, data.promise));
+      }
+      else
+        console.log(Object.assign(res, data.promise));
+
+      // this.loader.open();
+      // this.crudService.Insertar(res, 'modeloplancontable/').subscribe(data => {
+      //   this.getItems('All', 0);
+      //   this.loader.close();
+      //   this.snack.open('Agregado!', 'OK', {duration: 4000});
+      // });
+
+
+    });
+
+
+    // this.CargarCuenta(data.data);
+
+
+    // dialogRef.afterClosed()
+    //   .subscribe(res => {
+    //     if (!res) {
+    //       // If user press cancel
+    //       return;
+    //     }
+    //     this.loader.open();
+    //     if (isNew) {
+    //       this.crudService.Insertar(res, "modeloplancontable/").subscribe(data => {
+    //         // this.getItems("All", 0);
+    //         this.loader.close();
+    //         this.snack.open('Agregado!', 'OK', { duration: 4000 })
+    //       })
+    //     } else {
+    //       this.crudService.Actualizar(data.ID, res, "modeloplancontable/").subscribe(data => {
+    //         //  this.getItems("All", 0);
+    //         this.loader.close();
+    //         this.snack.open('Actualizado!', 'OK', { duration: 4000 })
+    //       })
+    //     }
+    //   });
   }
 
   deleteItem(row) {
-    this.confirmService.confirm({ message: `Eliminar ${row.Etiqueta}?` })
+    this.confirmService.confirm({message: `Eliminar ${row.Etiqueta}?`})
       .subscribe(res => {
         if (res) {
           this.loader.open();
-          this.crudService.Eliminar(row.ID, "modeloplancontable/").subscribe(data => {
+          this.crudService.Eliminar(row.ID, 'modeloplancontable/').subscribe(data => {
             // this.getItems("All", 0);
             this.loader.close();
-            this.snack.open('Eliminado!', 'OK', { duration: 4000 })
-          })
+            this.snack.open('Eliminado!', 'OK', {duration: 4000});
+          });
         }
-      })
+      });
 
   }
-  nodeSelect(event) {
-    console.log(event.node);
-  }
 
-  nodeUnselect(event) {
-    //console.log(event.node);
-  }
+  // nodeSelect(event) {
+  //   console.log(event.node);
+  // }
+  //
+  // nodeUnselect(event) {
+  //   this.selectedFile = null;
+  // }
 
 
 }
--- src/app/modulos/configurar/plancontable/popup/popup.component.ts
+++ src/app/modulos/configurar/plancontable/popup/popup.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { CrudService } from '../../../../shared/servicios/crud.service';
-import { CuentaContable } from '../cuentacontable.model';
+import {Component, OnInit, Inject} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
+import {CrudService} from '../../../../shared/servicios/crud.service';
+import {CuentaContable} from '../cuentacontable.model';
 
 @Component({
   selector: 'app-popup',
@@ -10,46 +10,46 @@ import { CuentaContable } from '../cuentacontable.model';
   styles: []
 })
 export class PopupComponentPC implements OnInit {
-  Grupo = "Detalle";
   Cuentas = [];
   public itemForm: FormGroup;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<PopupComponentPC>,
-    private fb: FormBuilder,
-    private crudService: CrudService,
-  ) {
-
-
-   }
+    private fb: FormBuilder) {
+  }
 
   ngOnInit() {
 
-    //if (this.data.title == 'Actualizar') {
-      this.crudService.ListarDatos("cuentacontable", "ID", this.data.payload.data).map((response) => {
-        return response.json() as CuentaContable[];
-      }).toPromise().then(x => {
-        this.data.payload = x;
-        console.log(this.data.payload);
-      })
+    if (Array.isArray(this.data.payload.promise)) {
+      this.newItemform(this.data.payload);
+    } else {
+      this.buildItemForm(this.data.payload);
+    }
 
-   // }
-    console.log(this.data.payload);
-    this.buildItemForm(this.data.payload);
   }
+
   buildItemForm(item) {
-   
     this.itemForm = this.fb.group({
-      NumeroCuenta: [item.NumeroCuenta || ''],
-      Etiqueta: [item.Etiqueta || '', Validators.required],
-      CuentaPadre: [{ value: item.label || '', disabled: true }],
-      //GrupoCuenta: [item.GrupoCuenta || 0],
-      GrupoCuenta: [{ value: this.Grupo || '', disabled: true }],
-      Estado: [item.Estado || false]
-    })
+      NumeroCuenta: [{value: item.promise.NumeroCuenta, disabled: true}],
+      Etiqueta: [item.promise.Etiqueta, Validators.required],
+      CuentaPadre: [{value: (item.parent != null) ? item.parent.label : '', disabled: true}],
+      GrupoCuenta: [{value: (item.children.length > 0) ? 'Grupo' : 'Detalle' || '', disabled: true}],
+      Estado: [item.promise.Estado || false]
+    });
+  }
+
+  newItemform(item) {
+    this.itemForm = this.fb.group({
+      NumeroCuenta: [ {value: (item.numerocuenta + '.' + item.promise[0].ncuenta) , disabled: true} ],
+      Etiqueta: ['', Validators.required],
+      CuentaPadre: [{value: item.label || '', disabled: true}],
+      GrupoCuenta: [{value: 'Detalle' || '', disabled: true}],
+      Estado: [true]
+    });
   }
 
   submit() {
-    this.dialogRef.close(this.itemForm.value)
+    this.dialogRef.close(this.itemForm.value);
   }
 }
