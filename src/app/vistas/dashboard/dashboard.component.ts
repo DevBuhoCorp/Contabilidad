a6ff6023700749fa405643fd95cea7e6ba65cdcd
Ronald <ronald.chica.2ai@gmail.com>
2018-10-29 16:51:59 -0500

    dashboard

--- src/app/vistas/dashboard/dashboard.component.ts
+++ src/app/vistas/dashboard/dashboard.component.ts
@@ -1,44 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../shared/servicios/crud.service';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.template.html'
 })
 export class DashboardComponent implements OnInit {
-  lineChartSteppedData: Array<any> = [{
-    data: [1, 8, 4, 8, 2, 2, 9],
-    label: 'Order',
-    borderWidth: 0,
-    fill: true,
-    // steppedLine: true
-  }, {
-    data: [6, 2, 9, 3, 8, 2, 1],
-    label: 'New client',
-    borderWidth: 1,
-    fill: true,
-    // steppedLine: true
-  }];
-  public lineChartLabels: Array<any> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July'];
   /*
-  * Full width Chart Options
-  */
-  public lineChartOptions: any = {
+   * Line Chart Options
+   */
+  sharedChartOptions: any = {
     responsive: true,
-    maintainAspectRatio: false,
+    // maintainAspectRatio: false,
     legend: {
       display: false,
       position: 'bottom'
-    },
+    }
+  };
+  chartColors: Array<any> = [{
+    backgroundColor: '#3f51b5',
+    borderColor: '#3f51b5',
+    pointBackgroundColor: '#3f51b5',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  }, {
+    backgroundColor: '#eeeeee',
+    borderColor: '#e0e0e0',
+    pointBackgroundColor: '#e0e0e0',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(77,83,96,1)'
+  }, {
+    backgroundColor: 'rgba(148,159,177,0.2)',
+    borderColor: 'rgba(148,159,177,1)',
+    pointBackgroundColor: 'rgba(148,159,177,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  }];
+   lineChartData: Array <any> = [{
+    data: [],
+    borderWidth: 1
+  }];
+  lineChartLabels:any=[];
+  lineChartOptions: any = Object.assign({
+    animation: false,
     scales: {
       xAxes: [{
-        display: false,
         gridLines: {
           color: 'rgba(0,0,0,0.02)',
           zeroLineColor: 'rgba(0,0,0,0.02)'
         }
       }],
       yAxes: [{
-        display: false,
         gridLines: {
           color: 'rgba(0,0,0,0.02)',
           zeroLineColor: 'rgba(0,0,0,0.02)'
@@ -49,151 +64,68 @@ export class DashboardComponent implements OnInit {
         }
       }]
     }
-  };
-
-  public lineChartColors: Array<any> = [{
-    backgroundColor: 'rgba(63, 81, 181, 0.16)',
-    borderColor: 'rgba(0,0,0,0)',
-    pointBackgroundColor: 'rgba(63, 81, 181, 0.4)',
-    pointBorderColor: 'rgba(0, 0, 0, 0)',
-    pointHoverBackgroundColor: 'rgba(63, 81, 181, 1)',
-    pointHoverBorderColor: 'rgba(148,159,177,0)'
-  }, {
-    backgroundColor: 'rgba(0, 0, 0, .08)',
-    borderColor: 'rgba(0,0,0,0)',
-    pointBackgroundColor: 'rgba(0, 0, 0, 0.06)',
-    pointBorderColor: 'rgba(0, 0, 0, 0)',
-    pointHoverBackgroundColor: 'rgba(0, 0, 0, 0.1)',
-    pointHoverBorderColor: 'rgba(0, 0, 0, 0)'
-  }];
+  }, this.sharedChartOptions);
   public lineChartLegend: boolean = false;
   public lineChartType: string = 'line';
-
-
-  // Chart grid options
-  doughnutChartColors1: any[] = [{
-    backgroundColor: ['#fff', 'rgba(0, 0, 0, .24)',]
+  lineChartPointsData: Array<any> = [{
+    data: [6, 5, 8, 8, 5, 5, 4],
+    label: 'Series A',
+    borderWidth: 1,
+    fill: false,
+    pointRadius: 10,
+    pointHoverRadius: 15,
+    showLine: false
+  }, {
+    data: [5, 4, 4, 2, 6, 2, 5],
+    label: 'Series B',
+    borderWidth: 1,
+    fill: false,
+    pointRadius: 10,
+    pointHoverRadius: 15,
+    showLine: false
   }];
-  doughnutChartColors2: any[] = [{
-    backgroundColor: ['rgba(0, 0, 0, .5)', 'rgba(0, 0, 0, .15)',]
-  }];
-  total1: number = 500;
-  data1: number = 200;
-  doughnutChartData1: number[] = [this.data1, (this.total1 - this.data1)];
-
-  total2: number = 600;
-  data2: number = 400;
-  doughnutChartData2: number[] = [this.data2, (this.total2 - this.data2)];
-  doughnutLabels = ['Spent', 'Remaining']
-  doughnutChartType = 'doughnut';
-  doughnutOptions: any = {
-    cutoutPercentage: 85,
-    responsive: true,
-    legend: {
-      display: false,
-      position: 'bottom'
+  lineChartPointsOptions: any = Object.assign({
+    scales: {
+      xAxes: [{
+        gridLines: {
+          color: 'rgba(0,0,0,0.02)',
+          zeroLineColor: 'rgba(0,0,0,0.02)'
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          color: 'rgba(0,0,0,0.02)',
+          zeroLineColor: 'rgba(0,0,0,0.02)'
+        },
+        ticks: {
+          beginAtZero: true,
+          suggestedMax: 9,
+        }
+      }]
     },
     elements: {
-      arc: {
-        borderWidth: 0,
+      point: {
+        pointStyle: 'rectRot',
       }
-    },
-    tooltips: {
-      enabled: true
     }
-  };
+  }, this.sharedChartOptions);
 
-  photos = [{
-    name: 'Photo 1',
-    url: 'assets/images/sq-15.jpg'
-  }, {
-    name: 'Photo 2',
-    url: 'assets/images/sq-8.jpg'
-  }, {
-    name: 'Photo 3',
-    url: 'assets/images/sq-9.jpg'
-  }, {
-    name: 'Photo 4',
-    url: 'assets/images/sq-10.jpg'
-  }, {
-    name: 'Photo 5',
-    url: 'assets/images/sq-11.jpg'
-  }, {
-    name: 'Photo 6',
-    url: 'assets/images/sq-12.jpg'
-  }]
-  tickets = [{
-    img: 'assets/images/face-1.jpg',
-    name: 'Mike Dake',
-    text: 'Excerpt pipe is used.',
-    date: new Date('07/12/2017'),
-    isOpen: true
-  }, {
-    img: 'assets/images/face-5.jpg',
-    name: 'Jhone Doe',
-    text: 'My dashboard is not working.',
-    date: new Date('07/7/2017'),
-    isOpen: false
-  }, {
-    img: 'assets/images/face-3.jpg',
-    name: 'Jhonson lee',
-    text: 'Fix stock issue',
-    date: new Date('04/10/2017'),
-    isOpen: false
-  }, {
-    img: 'assets/images/face-4.jpg',
-    name: 'Mikie Jyni',
-    text: 'Renew my subscription.',
-    date: new Date('07/7/2017'),
-    isOpen: false
-  }]
-  // users
-  users = [
-    {
-      "name": "Snow Benton",
-      "membership": "Paid Member",
-      "phone": "+1 (956) 486-2327",
-      "photo": "assets/images/face-4.jpg",
-      "address": "329 Dictum Court, Minnesota",
-      "registered": "2016-07-09"
-    },
-    {
-      "name": "Kay Sellers",
-      "membership": "Paid Member",
-      "phone": "+1 (929) 406-3172",
-      "photo": "assets/images/face-2.jpg",
-      "address": "893 Garden Place, American Samoa",
-      "registered": "2017-02-16"
-    }
-  ]
+  constructor(private crudService: CrudService) { }
+  ngOnInit() {
+    this.getdata();
 
-  projects = [{
-    name: 'User Story',
-    user: 'Watson Joyce',
-    progress: 100,
-    leader: 'Snow Benton'
-  }, {
-    name: 'Design Data Model',
-    user: 'Morris Adams',
-    progress: 30,
-    leader: 'Watson Joyce'
-  }, {
-    name: 'Develop CR Algorithm',
-    user: 'Jhone Doe',
-    progress: 70,
-    leader: 'Ada Kidd'
-  }, {
-    name: 'Payment Module',
-    user: 'Ada Kidd',
-    progress: 50,
-    leader: 'Snow Benton'
-  }, {
-    name: 'Discount Module',
-    user: 'Workman Floyd',
-    progress: 50,
-    leader: 'Robert Middleton'
-  }]
-
-  constructor() { }
-  ngOnInit() { }
+  }
+  async getdata() {
+    let clone = JSON.parse(JSON.stringify(this.lineChartData));
+    
+    
+    let datos:any = await this.crudService.SeleccionarAsync("transpormes");
+    this.lineChartData = datos.map(i=>{
+        clone[0].data.push(i.data);
+        //this.lineChartData[0].data.push(i.data);
+        this.lineChartLabels.push(i.label);
+    })
+    this.lineChartData = clone
+    
+  }
 }
